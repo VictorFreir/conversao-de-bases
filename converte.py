e7f9ac06557027aa21ea1d597aa606c388168a47
OEM Configuration (temporary user) <victorvfreire@gmail.com>
2022-02-16 19:00:14 -0300

    convert to decimal

--- converte.py
+++ converte.py
@@ -4,3 +4,17 @@ if base_entrada == 10:
     numero_em_decimal = numero
 
 else: #Converte para decimal
+    numero_string = str(numero)
+    numero_string = numero_string[::-1]
+    tamanho_numero = len(numero_string)
+    soma = 0
+    for index in tamanho_numero:
+        algarismo =  int(numero_string[index])
+        soma += (base_entrada**index)*algarismo
+    numero_decimal = int(soma)
+
+if base_saida == 10:
+    numero_final = numero_decimal
+
+elif:
+    
